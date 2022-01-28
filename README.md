b63df8b20a659dbfcfe83492b796e1029e12002b
markinascimento <markinhonascimento50@gmail.com>
2022-01-28 16:47:17 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# utils-format-brl
-Pacote para formatação no padrão Brasileiro
+
+# Funções úteis para formataçaõ no padrão do BRASIL
+
+
+## Funções
+
+- handlerToFormatCPF
+
+- handlerToFormatCPNJ
+
+- handlerToFormatPhone
+
+- handlerToFormatMoneyBRL
+
+
+
+## Instalação
+
+Instalar o pacote no projeto usando o npm
+
+```bash
+  npm install utils-format-brl
+```
+    
+## Use
+
+```javascript
+import { handlerToFormatCPF } from 'utils-format-brl'
+
+function Example() {
+  console.log(handlerToFormatCPF(12345678912));
+  // 123.456.789-12
+}
+```
+
+```javascript
+import { handlerToFormatCPNJ } from 'utils-format-brl'
+
+function Example() {
+  console.log(handlerToFormatCPNJ(703126280020108));
+  // 70.312.628/0020-108
+}
+```
+
+```javascript
+import { handlerToFormatPhone } from 'utils-format-brl'
+
+function Example() {
+  console.log(handlerToFormatPhone(99999929993));
+  // (99) 99992-9993
+}
+```
+
+```javascript
+import { handlerToFormatMoneyBRL } from 'utils-format-brl'
+
+function Example() {
+  console.log(handlerToFormatMoneyBRL(1212.29));
+  // R$ 1.212,29
+}
+```
---
+++ index.mjs
@@ -0,0 +1,60 @@
+function handlerToFormatCEP(value) {
+  if(String(value).length < 8){
+    return 'Por Favor, insira 8 dígitos!'
+  
+  }else {
+    return String(value)
+      .replace(/\D/g, "")
+      .slice(0, 8)
+      .replace(/(\d{5})(\d)/, "$1-$2");
+  }
+}
+
+function handlerToFormatCPF(value){
+  if(String(value).length < 8){
+    return 'Por Favor, insira 11 dígitos!'
+  
+  }else {
+    return String(value)
+      .slice(0, 11)
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
+}
+
+function handlerToFormatCPNJ(value) { 
+  if(String(value).length < 15){
+    return 'Por Favor, insira 15 dígitos!';
+
+  }else { 
+    return String(value)
+      .slice(0, 15)
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
+  }
+}
+
+function handlerToFormatPhone(value){
+  if(String(value).length < 11){
+    return 'Por Favor, insira 11 dígitos!';
+
+  }else {
+    return String(value)
+      .slice(0, 11)
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+  }
+}
+
+function handlerToFormatMoneyBRL(value){
+  return value.toLocaleString('pt-br', {
+    style: 'currency',
+    currency: 'BRl'    
+  });
+}
+
+console.log(handlerToFormatMoneyBRL(1212.29));
+
+export { 
+  handlerToFormatCEP, handlerToFormatCPF, handlerToFormatCPNJ, handlerToFormatPhone,handlerToFormatMoneyBRL, 
+}; 
